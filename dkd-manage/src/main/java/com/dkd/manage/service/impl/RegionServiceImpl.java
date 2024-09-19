1798917bd846b83d7368082e6ebc4afe5d49dcb2
xianzicai <xianzhiyong763@gmail.com>
2024-09-19 17:38:42 +0800

    点位管理相关代码改造

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
@@ -2,6 +2,7 @@ package com.dkd.manage.service.impl;
 
 import java.util.List;
 import com.dkd.common.utils.DateUtils;
+import com.dkd.manage.domain.vo.RegionVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.RegionMapper;
@@ -92,5 +93,15 @@ public class RegionServiceImpl implements IRegionService
     public int deleteRegionById(Long id)
     {
         return regionMapper.deleteRegionById(id);
+    }
+
+    /**
+     * 查询区域管理列表VO
+     * @param region
+     * @return RegionVO 集合
+     */
+    @Override
+    public List<RegionVO> selectRegionListVO(Region region) {
+        return regionMapper.selectRegionListVO(region);
     }
 }
