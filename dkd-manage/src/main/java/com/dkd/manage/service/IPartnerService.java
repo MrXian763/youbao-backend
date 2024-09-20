23f41bebbc857048ee2b47c44b7f0b4e7170b7f5
xianzicai <xianzhiyong763@gmail.com>
2024-09-20 19:44:09 +0800

    合作商改造

--- dkd-manage/src/main/java/com/dkd/manage/service/IPartnerService.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/IPartnerService.java
@@ -2,6 +2,7 @@ package com.dkd.manage.service;
 
 import java.util.List;
 import com.dkd.manage.domain.Partner;
+import com.dkd.manage.domain.vo.PartnerVO;
 
 /**
  * 合作商管理Service接口
@@ -58,4 +59,11 @@ public interface IPartnerService
      * @return 结果
      */
     public int deletePartnerById(Long id);
+
+    /**
+     * 查询合作商管理列表VO
+     * @param partner
+     * @return 合作商列表VO集合
+     */
+    public List<PartnerVO> selectPartnerListVO(Partner partner);
 }
--- dkd-manage/src/main/java/com/dkd/manage/service/impl/PartnerServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/PartnerServiceImpl.java
@@ -3,6 +3,7 @@ package com.dkd.manage.service.impl;
 import java.util.List;
 import com.dkd.common.utils.DateUtils;
 import com.dkd.common.utils.SecurityUtils;
+import com.dkd.manage.domain.vo.PartnerVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.PartnerMapper;
@@ -95,5 +96,15 @@ public class PartnerServiceImpl implements IPartnerService
     public int deletePartnerById(Long id)
     {
         return partnerMapper.deletePartnerById(id);
+    }
+
+    /**
+     * 查询合作商管理列表VO
+     * @param partner
+     * @return 合作商列表VO集合
+     */
+    @Override
+    public List<PartnerVO> selectPartnerListVO(Partner partner) {
+        return partnerMapper.selectPartnerListVO(partner);
     }
 }
