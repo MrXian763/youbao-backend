901673db3d0e81cb9a436b39a3541e5d18b3cf58
xianzicai <xianzhiyong763@gmail.com>
2024-09-23 21:59:09 +0800

    人员管理-人员表冗余字段同步存储管理

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/EmpServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/EmpServiceImpl.java
@@ -2,6 +2,9 @@ package com.dkd.manage.service.impl;
 
 import java.util.List;
 import com.dkd.common.utils.DateUtils;
+import com.dkd.manage.domain.Role;
+import com.dkd.manage.mapper.RegionMapper;
+import com.dkd.manage.mapper.RoleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.EmpMapper;
@@ -19,6 +22,10 @@ public class EmpServiceImpl implements IEmpService
 {
     @Autowired
     private EmpMapper empMapper;
+    @Autowired
+    private RoleMapper roleMapper;
+    @Autowired
+    private RegionMapper regionMapper;
 
     /**
      * 查询人员列表
@@ -53,6 +60,10 @@ public class EmpServiceImpl implements IEmpService
     @Override
     public int insertEmp(Emp emp)
     {
+        emp.setRegionName(regionMapper.selectRegionById(emp.getRegionId()).getRegionName());
+        Role role = roleMapper.selectRoleByRoleId(emp.getRoleId());
+        emp.setRoleName(role.getRoleName());
+        emp.setRoleCode(role.getRoleCode());
         emp.setCreateTime(DateUtils.getNowDate());
         return empMapper.insertEmp(emp);
     }
@@ -66,6 +77,10 @@ public class EmpServiceImpl implements IEmpService
     @Override
     public int updateEmp(Emp emp)
     {
+        emp.setRegionName(regionMapper.selectRegionById(emp.getRegionId()).getRegionName());
+        Role role = roleMapper.selectRoleByRoleId(emp.getRoleId());
+        emp.setRoleName(role.getRoleName());
+        emp.setRoleCode(role.getRoleCode());
         emp.setUpdateTime(DateUtils.getNowDate());
         return empMapper.updateEmp(emp);
     }
